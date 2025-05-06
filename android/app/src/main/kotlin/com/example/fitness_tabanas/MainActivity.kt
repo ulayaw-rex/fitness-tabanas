440c07683709362a0efc2f022305fa62bae45819
ulayaw-rex <jhonrextabanas27@gmail.com>
2025-05-06 16:06:42 +0800

    First Commit

---
+++ android/app/src/main/kotlin/com/example/fitness_tabanas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fitness_tabanas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(
+      appBar: appBar(),
+    );
+    return scaffold;
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text('Breakfast', style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold)),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+      child: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg', height: 20, width: 20,),
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
+        ),
+      ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+
+          },
+        child: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        width: 37,
+        child: SvgPicture.asset('assets/icons/dots.svg', height: 5, width: 5,),
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
+        ),
+      ),
+        ),
+      ],
+    );
+  }
+}
